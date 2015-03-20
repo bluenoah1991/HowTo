9aa27fe5acea3657184893b8f6449dad01ab7558
codemeow2 <yorick.zhou@gmail.com>
2015-03-20 12:45:32 +0800

    add something

--- debug-tomcat7.sh
+++ debug-tomcat7.sh
@@ -4,7 +4,7 @@ if [ $# > 0 ]; then
   fileName=$1
   rm /var/lib/tomcat7/work/Catalina/ -rf
   rm /var/lib/tomcat7/webapps/${fileName} -rf
-  rm /var/lib/tomcat7/webapps/${fileName%.war*}
+  rm /var/lib/tomcat7/webapps/${fileName%.war*} -rf
   cp ${fileName} /var/lib/tomcat7/webapps/${fileName}
   /usr/share/tomcat7/bin/catalina.sh stop
   /usr/share/tomcat7/bin/catalina.sh jpda start
