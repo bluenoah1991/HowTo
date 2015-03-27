160c699f27521df15ea9bad75ab1e1b4b3aca7a2
codemeow2 <yorick.zhou@gmail.com>
2015-03-27 08:54:19 +0800

    add something

--- bdcrawler/src/job_song.py
+++ bdcrawler/src/job_song.py
@@ -95,7 +95,7 @@ def Start(db_, artist_list):
                                             if dwn_lrc[0].transfer(lrclink, songId, 'text/plain'):
                                                 break
                                             elif i == 2:
-                                                db_.add_failed(songlink, songId, 'text/plain', 2)
+                                                db_.add_failed(lrclink, songId, 'text/plain', 2)
                                     Order_[0] = Order_[0] + 1
                             #Order_[0] = Order_[0] + 1
                             print 'song %d has been saved.' % songId
