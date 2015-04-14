823bfbf16a84a2014e53e3e9ae0b59fa39b333f5
CodeMeow <codemeow5@gmail.com>
2015-04-14 17:02:24 +0800

    add something

---
+++ sshkey_setting.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir ~/.ssh
+cp id_rsa.pub ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/authorized_keys
+sed -i '/^#PasswordAuthentication/cPasswordAuthentication no' /etc/ssh/sshd_config
+sed -i '/^#AuthorizedKeyFile/s/#//' /etc/ssh/sshd_config
+/etc/init.d/ssh restart
+
