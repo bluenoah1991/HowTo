50e850e2205aa5c80345858da6842ba51306bc1f
codemeow2 <yorick.zhou@gmail.com>
2015-04-16 09:41:00 +0800

    Update sshkey_setting.sh

--- sshkey_setting.sh
+++ sshkey_setting.sh
@@ -4,6 +4,6 @@ mkdir ~/.ssh
 cp id_rsa.pub ~/.ssh/authorized_keys
 chmod 644 ~/.ssh/authorized_keys
 sed -i '/^#PasswordAuthentication/cPasswordAuthentication no' /etc/ssh/sshd_config
-sed -i '/^#AuthorizedKeyFile/s/#//' /etc/ssh/sshd_config
+sed -i '/^#AuthorizedKeysFile/s/#//' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
 
