163bb3218adea20b3ddddd48989bdf8fdebde827
codemeow2 <yorick.zhou@gmail.com>
2015-04-21 16:41:45 +0800

    Update cobbler-install.sh

--- cobbler-install.sh
+++ cobbler-install.sh
@@ -4,6 +4,7 @@
 
 # Disable SELinux
 # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security-Enhanced_Linux/sect-Security-Enhanced_Linux-Enabling_and_Disabling_SELinux-Disabling_SELinux.html
+# vim /etc/selinux/config
 # SELINUX=disabled
 
 yum update
