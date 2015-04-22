2997b267726d500414f4c9614ab79b6ffc7dea2e
CodeMeow <codemeow5@gmail.com>
2015-04-22 16:22:46 +0800

    add something

--- op-ntp-install.sh
+++ op-ntp-install.sh
@@ -5,9 +5,14 @@ apt-get install ntp -y
 
 cp /etc/ntp.conf /etc/ntp.conf.bak
 
-sed -i "/^server/s/$/ iburst/" /etc/ntp.conf
-sed -i "/^restrict -4/s/nopeer noquery//" /etc/ntp.conf
-sed -i "/^restrict -6/s/nopeer noquery//" /etc/ntp.conf
+if [ $# -eq 0 ]; then
+  sed -i "/^server/s/$/ iburst/" /etc/ntp.conf
+  sed -i "/^restrict -4/s/nopeer noquery//" /etc/ntp.conf
+  sed -i "/^restrict -6/s/nopeer noquery//" /etc/ntp.conf
+else
+  sed -i "/^server/s/^/#/" /etc/ntp.conf
+  echo "server $1 iburst" >> /etc/ntp.conf
+fi
 
 service ntp restart
 
