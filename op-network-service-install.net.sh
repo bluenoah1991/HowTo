7fd7a7059f8e4669991774cad45d2680c2b101d9
CodeMeow <codemeow5@gmail.com>
2015-04-23 20:26:12 +0800

    add something

---
+++ op-network-service-install.net.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i "/^#net.ipv4.ip_forward=1/s/#//" /etc/sysctl.conf
+sed -i "/^#net.ipv4.conf.default.rp_filter=1/cnet.ipv4.conf.default.rp_filter=0" /etc/sysctl.conf
+sed -i "/^#net.ipv4.conf.all.rp_filter=1/cnet.ipv4.conf.all.rp_filter=0" /etc/sysctl.conf
+
+sysctl -p
+
+apt-get install neutron-plugin-ml2 neutron-plugin-openvswitch-agent \
+  neutron-l3-agent neutron-dhcp-agent
+
+
