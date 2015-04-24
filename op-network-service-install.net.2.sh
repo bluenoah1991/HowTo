2372628812c9467ca74ea6e6c2c244edac13633c
CodeMeow <codemeow5@gmail.com>
2015-04-24 08:55:09 +0800

    add something

--- op-network-service-install.net.2.sh
+++ op-network-service-install.net.2.sh
@@ -2,12 +2,8 @@
 
 METADATA_SECRET=123456 # from op-network-service-install.net.sh
 
+echo "service_metadata_proxy = True" >> /etc/nova/nova.conf # just once
+echo "metadata_proxy_shared_secret = ${METADATA_SECRET}" >> /etc/nova/nova.conf # just once
 
-
-
-
-
-
-
-
+service nova-api restart # run command line when adding compute nodes
 
