b29ea24805f42b61b265b712e3a2656d143da950
codemeow5 <codemeow@yahoo.com>
2015-07-29 09:33:12 +0800

    Update mongodb-install.sh

--- mongodb-install.sh
+++ mongodb-install.sh
@@ -22,7 +22,7 @@ apt-get install php5-dev -y
 
 echo 'start install mongo php driver...'
 
-pecl install mongo
+printf '\n' | pecl install mongo
 
 echo 'extension=mongo.so' >> /etc/php5/apache2/php.ini
 
@@ -32,8 +32,8 @@ wget ${RockMongo_URL} --output-document=/tmp/rockmongo.tar.gz
 
 tar -zxvf /tmp/rockmongo.tar.gz -C /var/www/html
 
+rockmongodir=`ls -l /var/www/html | grep rockmongo | rev | cut -d ' ' -f1 | rev`
+
+ln -s /var/www/html/$rockmongodir /var/www/html/rockmongo
+
 echo 'success'
-
-
-
-
