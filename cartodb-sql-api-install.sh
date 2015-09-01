d2a4f1e4d4262506c345729ebc79c28699e679b5
codemeow5 <codemeow@yahoo.com>
2015-09-01 14:08:19 +0800

    Update cartodb-sql-api-install.sh

--- cartodb-sql-api-install.sh
+++ cartodb-sql-api-install.sh
@@ -12,6 +12,12 @@ createdb -O ${PGUSER} ${PGDB}
 EOF
 
 apt-get install -y postgis postgresql-9.3-postgis-2.1
+
+cd /etc/postgresql/9.3/main
+sed -i "/^[^#]/s/peer$/trust/" /etc/postgresql/9.3/main/pg_hba.conf
+sed -i "/^[^#]/s/md5$/trust/" /etc/postgresql/9.3/main/pg_hba.conf
+/etc/init.d/postgresql restart
+
 sudo -u postgres bash << EOF
 psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" ${PGDB}
 EOF
