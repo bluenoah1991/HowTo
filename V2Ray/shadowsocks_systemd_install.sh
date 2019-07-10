df4dfbc25c34b8f8d48b6c6d28c34ef2fb0b4a66
Zhou YangYang <codemeow@icloud.com>
2019-07-10 10:20:47 +0800

    Update shadowsocks_systemd_install.sh

--- V2Ray/shadowsocks_systemd_install.sh
+++ V2Ray/shadowsocks_systemd_install.sh
@@ -64,7 +64,7 @@ fi
 # configure to automatically start up at boot
 if [ -e /etc/rc.local ]; then
 if ! grep -q ssserver /etc/rc.local; then
-COMMAND="/usr/bin/ss-server -c /etc/shadowsocks.json 2>&1 &"
+COMMAND="/usr/bin/ss-server -c /etc/shadowsocks.json -f /var/run/shadowsocks-v2ray.pid"
 sed -i "/^exit\\ 0/i\\${COMMAND}\\n" /etc/rc.local
 # start server
 ${COMMAND}
